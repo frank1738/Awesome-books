18e7ed36c3f52ed0ae5a9984a67c27ead30e4cf8
frank1738 <98453979+frank1738@users.noreply.github.com>
2022-04-20 22:01:44 +0300

    add features

--- index.js
+++ index.js
@@ -143,3 +143,21 @@ window.addEventListener('DOMContentLoaded', () => {
     bookAuthor.value = '';
   });
 });
+
+function time() {
+  const siteDate = document.querySelector('.time');
+  const date = new Date();
+  const locale = navigator.language;
+  const options = {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: 'false',
+  };
+  siteDate.textContent = `${date.toLocaleTimeString(locale, options)}`;
+}
+
+setInterval(time, 1000);
